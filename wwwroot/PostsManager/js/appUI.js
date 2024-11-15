8811a20e44f3a64470cda87204e472e5c1d2d580
julie-beaucage <201386196@edu.clg.qc.ca>
2024-11-15 18:52:54 -0500

    ok

--- wwwroot/PostsManager/js/appUI.js
+++ wwwroot/PostsManager/js/appUI.js
@@ -65,8 +65,9 @@ function doSearch() {
 }
 
 function showPosts() {
-  $("#actionTitle").text("Liste des favoris");
+  $("#actionTitle").text("Chouettes Nouvelles");
   $("#scrollPanel").show();
+  $("#show_category").text(`${selectedCategory}`); 
   $("#abort").hide();
   $("#postForm").hide();
   $("#aboutContainer").hide();
@@ -102,45 +103,41 @@ function updateDropDownMenu() {
   let DDMenu = $("#DDMenu");
   let selectClass = selectedCategory === "" ? "fa-check" : "fa-fw";
   DDMenu.empty();
-  DDMenu.append(
-    $(`
-        <div class="dropdown-item menuItemLayout" id="allCatCmd">
-            <i class="menuIcon fa ${selectClass} mx-2"></i> Toutes les catégories
-        </div>
-        `)
-  );
+  DDMenu.append($(`
+    <div class="dropdown-item menuItemLayout" id="allCatCmd">
+      <i class="menuIcon fa ${selectClass} mx-2"></i> Toutes les catégories
+    </div>
+    `));
   DDMenu.append($(`<div class="dropdown-divider"></div>`));
   categories.forEach((category) => {
     selectClass = selectedCategory === category ? "fa-check" : "fa-fw";
-    DDMenu.append(
-      $(`
-            <div class="dropdown-item menuItemLayout category" id="allCatCmd">
-                <i class="menuIcon fa ${selectClass} mx-2"></i> ${category}
-            </div>
-        `)
-    );
+    DDMenu.append($(`
+      <div class="dropdown-item menuItemLayout category" id="allCatCmd">
+        <i class="menuIcon fa ${selectClass} mx-2"></i> ${category}
+      </div>
+    `));
   });
   DDMenu.append($(`<div class="dropdown-divider"></div> `));
-  DDMenu.append(
-    $(`
-        <div class="dropdown-item menuItemLayout" id="aboutCmd">
-            <i class="menuIcon fa fa-info-circle mx-2"></i> À propos...
-        </div>
-        `)
-  );
+  DDMenu.append($(`
+    <div class="dropdown-item menuItemLayout" id="aboutCmd">
+      <i class="menuIcon fa fa-info-circle mx-2"></i> À propos...
+    </div>
+    `) );
   $("#aboutCmd").on("click", function () {
-     renderAbout();
+    renderAbout();
   });
   $("#allCatCmd").on("click", function () {
-     showPosts();
-     selectedCategory = "";
-      updateDropDownMenu();
-      pageManager.reset();
+    showPosts();
+    selectedCategory = "";
+    updateDropDownMenu();
+    $("#show_category").text("Toutes les catégories"); 
+    pageManager.reset();
   });
   $(".category").on("click", function () {
      showPosts();
      selectedCategory = $(this).text().trim();
      updateDropDownMenu();
+     $("#show_category").text(`${selectedCategory}`); 
      pageManager.reset();
   });
 }
@@ -169,27 +166,35 @@ async function renderPosts(queryString) {
     if (!API_Posts.error) {
         currentETag = response.ETag;
         let Posts = response.data;
+        console.log(Posts.length);
         if (Posts.length > 0) {
             Posts.forEach((Post) => {
               $("#itemsPanel").append(renderPost(Post));
-        });
-        $(".editCmd").off();
-        $(".editCmd").on("click", function () {
-            renderEditPostForm($(this).attr("editPostId"));
-        });
-        $(".deleteCmd").off();
-        $(".deleteCmd").on("click", function () {
-            renderDeletePostForm($(this).attr("deletePostId"));
-        });
-        } else {
-            //$("#itemsPanel").html('<div class="noPostsMessage">Aucun post disponible</div>');
-            endOfData = true;
-        }
-    } else {
-        renderError(API_Posts.currentHttpError);
-    }
-    removeWaitingGif();
-    return endOfData;
+            });
+            $(".editCmd").off();
+            $(".editCmd").on("click", function () {
+                renderEditPostForm($(this).attr("editPostId"));
+            });
+            $(".deleteCmd").off();
+            $(".deleteCmd").on("click", function () {
+                renderDeletePostForm($(this).attr("deletePostId"));
+            });
+          } else if(isReset) {
+            $("#itemsPanel").html(
+                `<div class="noPostsMessage">Aucun post dans la catégorie "${selectedCategory || "toutes les catégories"}" avec la recherche "${search || "aucun mot-clé"}".</div>`
+            );//{
+            /*if (selectedCategory || search) {
+              $("#itemsPanel").html(
+                  `<div class="noPostsMessage">Aucun post dans la catégorie "${selectedCategory || "toutes les catégories"}" avec la recherche "${search || "aucun mot-clé"}".</div>`
+              );
+              }*/
+              endOfData = true;
+          }
+      } else {
+          renderError(API_Posts.currentHttpError);
+      }
+      removeWaitingGif();
+      return endOfData;
 }
 
 function renderError(message) {
@@ -231,29 +236,36 @@ async function renderDeletePostForm(id) {
     let response = await API_Posts.Get(id);
     if (!API_Posts.error) {
         let Post = response.data;
+        const formattedDate = formatDate(Post.Creation);
         if (Post !== null) {
-            $("#PostForm").append(`
+            $("#postForm").append(`
             <div class="PostdeleteForm">
-                <h4>Effacer le favori suivant?</h4>
+                <h4>Effacer le post suivant?</h4>
                 <br>
-                <div class="PostRow" id=${Post.Id}">
-                    <div class="PostContainer noselect">
-                        <div class="PostLayout">
-                            <div class="Post">
-                                <span class="PostTitle">${Post.Title}</span>
-                            </div>
-                            <span class="PostCategory">${Post.Category}</span>
-                        </div>
-                        <div class="PostCommandPanel">
-                            <span class="editCmd cmdIcon fa fa-pencil" editPostId="${Post.Id}" title="Modifier ${Post.Title}"></span>
-                            <span class="deleteCmd cmdIcon fa fa-trash" deletePostId="${Post.Id}" title="Effacer ${Post.Title}"></span>
-                        </div>
+                <div class="PostRow" id='${Post.Id}'>
+                  <div class="PostContainer noselect">
+                    <div class="PostLayout">
+                      <div class="post-cover" style="background-image:url('${Post.Image}')"></div>
                     </div>
-                </div>   
+                    <div class ="post-body">
+                      <div class="post-title">
+                        <span class="PostTitle">${Post.Title}</span>
+                      </div>
+                      <span class="PostCategory">${Post.Category}</span>
+                      <div class="post-summary">
+                        <p>${Post.Text}</span>
+                      </div>
+                      <hr>
+                   </div>
+                   <div class="post-footer">
+                     <span> Publié: ${formattedDate}</span>  
+                    </div>
+                  </div>
+                </div>  
                 <br>
                 <input type="button" value="Effacer" id="deletePost" class="btn btn-primary">
                 <input type="button" value="Annuler" id="cancel" class="btn btn-secondary">
-            </div>    
+              </div>   
             `);
         $("#deletePost").on("click", async function () {
             await API_Posts.Delete(Post.Id);
@@ -404,7 +416,12 @@ function renderPost(Post) {
      <div class="PostRow" id='${Post.Id}'>
         <div class="PostContainer noselect">
             <div class="PostLayout">
-              <div class="post-cover" style="background-image:url('${Post.Image}')"></div>
+              <div class="post-cover">
+                <img src="${Post.Image}" alt="${Post.Title}" class="post-cover-img" />
+              </div>            </div>
+            <div class="post-icons">
+              <span class="editCmd cmdIcon fa fa-pencil" editPostId="${Post.Id}"title="Modifier ${Post.Title}"></span>
+              <span class="deleteCmd cmdIcon fa fa-trash" deletePostId="${Post.Id}"title="Effacer ${Post.Title}"></span>
             </div>
                 <div class ="post-body">
                    <div class="post-title">
@@ -417,12 +434,8 @@ function renderPost(Post) {
                 <hr>
             </div>
             <div class="post-footer">
-            <ul>
-                <li> Publié: ${formattedDate}</li>  
-                <li class="editCmd cmdIcon fa fa-pencil" editPostId="${Post.Id}" title="Modifier ${Post.Title}"></li>
-                <li class="deleteCmd cmdIcon fa fa-trash" deletePostId="${Post.Id}" title="Effacer ${Post.Title}"></li>
-            </ul>
-                </div>
+                <span> Publié: ${formattedDate}</span>  
+              </div>
         </div>
     </div>           
     `);
